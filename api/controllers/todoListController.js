f3fcbf3338058e68463ce5f21ee7ea32af895cf5
Juan Li Puma <juanlipuma94@gmail.com>
2018-11-25 23:03:42 -0500

    Initial commit
    - Clone and adapt repo https://github.com/hayjay/node-api
    - Repo is based on tutorial from https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
    - Configure eslint
    - Configure Editorconfig
    - Add dependencies
    - Add .nvmrc for consistent node versions
    - Add barebones REST API

---
+++ api/controllers/todoListController.js
@@ -0,0 +1,54 @@
+// in this controller, we would be writing five different function
+// *list all task
+// *read a task
+// *create a task
+// *update a task
+// *delete a task
+// Each of these functions uses different mongoose methods such as
+// find,
+// findById,
+// findOneAndUpdate,
+// save and remove.
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+const Task = mongoose.model('Tasks')
+
+// function for getting all available tasks
+exports.list_all_tasks = function (req, res) {
+  Task.find({}, function (err, task) {
+    if (err) { res.send(err).status(200) }
+    res.json(task)
+  })
+}
+
+exports.create_a_task = function (req, res) {
+  const task = new Task(req.body)
+  task.save(function (err, task) { // call the save method on the instance of the model in a callback way
+    if (err) { res.send(err) }
+    res.json(task)
+  })
+}
+
+exports.read_a_task = function (req, res) {
+  Task.findById(req.params.taskId, function (err, task) {
+    if (err) { res.send(err) }
+    res.json(task)
+  })
+}
+
+exports.update_a_task = function (req, res) {
+  Task.findOneAndUpdate({ _id: req.params.taskId }, req.body, { new: true }, function (err, task) {
+    if (err) { res.send(err) }
+    res.json(task)
+  })
+}
+
+exports.delete_a_task = function (req, res) {
+  Task.remove({ _id: req.params.taskId }, function (err, res) {
+    if (err) { res.send(err) }
+    res.json({ message: 'Task successfully deleted' })
+  })
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 4000
+
+const mongoose = require('mongoose')
+
+const Task = require('./api/models/todoListModel')
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise
+// connect directly to mongoose
+mongoose.connect('mongodb://localhost/spi', { useNewUrlParser: true }).then(
+  (res) => {
+    console.log('Successfully connected to the database.')
+  }
+).catch(() => {
+  console.log('Conntection to database failed.')
+})
+app.use(cors())
+app.use(bodyParser.json())
+
+// parse incoming requests
+app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+const routes = require('./api/routes/todoListRoute') // importing route
+routes(app)
+console.log(routes)
+// register the route file
+
+app.get('/', (req, res) => {
+  res.send('hey').status(200)
+})
+
+// middleware added to check if user enters not found route
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+})
+
+app.listen(port)
+
+console.log(`spi-api server started on port ${port}`)
