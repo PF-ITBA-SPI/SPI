2cd4c39d20595abf8b9740c0395761f39f945e44
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2019-10-25 09:35:52 -0300

    Add endpoint to calculate error of samples (#10)
    * Add endpoint to calculate error of samples
    
    * Renames
    
    * Simplify object assign
    
    * Some improvements, fixes
    
    - Filter NaN values for calculating mean
    - Avoid duplicate code
    - Add console.debug
    - Avoid modifying original array in `calculateLocationFilteringSample`
    - Fix some linter errors

--- api/controllers/location.js
+++ api/controllers/location.js
@@ -7,6 +7,7 @@ const Sample = mongoose.model('Sample')
 const DEFAULT_RSSI = 0
 const K1 = 10
 const K2 = 10
+const MIN_SAMPLES_FOR_POSITION = 3
 
 module.exports = {
   getLocation: async (req, res) => {
@@ -35,16 +36,67 @@ module.exports = {
         return res.status(404)
       }
       const filteredSampleId = req.params.sampleId
-      const filteredSampleIndex = samples.findIndex(sample => sample._id.equals(filteredSampleId))
-      if (filteredSampleIndex === -1) {
-        return res.status(404).send('Sample not found')
+      const location = calculateLocationFilteringSample(samples, filteredSampleId)
+      if (location === -1) {
+        res.status(404).send('Sample not found')
+      } else {
+        res.json(location)
       }
-      const filteredSample = samples.splice(filteredSampleIndex, 1)[0]
-      res.json(calculateLocation(samples, filteredSample.fingerprint))
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  },
+
+  getLocationError: async (req, res) => {
+    const query = Sample.find({})
+    query.lean()
+    try {
+      const samples = await query.exec()
+      if (samples === null) {
+        return res.status(404)
+      }
+      console.debug(`Calculating error for ${samples.length} samples...`)
+      var distances = {}
+      samples.forEach((sample) => {
+        const filteredSampleId = sample._id
+        const location = calculateLocationFilteringSample(samples, filteredSampleId)
+        distances[filteredSampleId] = getDistanceFromLatLonInKm(location.latitude, location.longitude, sample.latitude, sample.longitude) * 1000
+      })
+      const filteredValues = Object.values(distances).filter(v => !isNaN(v)) // Exclude NaN
+      const errorMean = filteredValues.reduce((acc, current) => acc + current, 0) / filteredValues.length
+      const meanSquaredError = filteredValues.reduce((acc, current) => acc + current * current, 0) / filteredValues.length
+      const result = { distances, errorMean, meanSquaredError }
+      res.json(result)
     } catch (err) {
       res.status(400).json(err)
     }
   }
+}
+
+function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) { // https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+  const R = 6371 // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1) // deg2rad below
+  const dLon = deg2rad(lon2 - lon1)
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  return R * c // Distance in km
+}
+
+function deg2rad (deg) {
+  return deg * (Math.PI / 180)
+}
+
+function calculateLocationFilteringSample (samples, filteredSampleId) {
+  const samplesCopy = [...samples]
+  const filteredSampleIndex = samplesCopy.findIndex(sample => sample._id.equals(filteredSampleId))
+  if (filteredSampleIndex === -1) {
+    return -1
+  }
+  const filteredSample = samplesCopy.splice(filteredSampleIndex, 1)[0]
+  return calculateLocation(samplesCopy, filteredSample.fingerprint)
 }
 
 function calculateLocation (samples, locationFingerprint) {
@@ -83,7 +135,7 @@ function calculateLocation (samples, locationFingerprint) {
   let R2 = R1.filter(sample => mainFingerprintSortedSSIDsByRSSI.slice(0, 3).includes(sample.sortedIdsByRSSI[0]))
   // Step 3: TODO we are not doing this so we take it as if #(R'') is always big enough
   // If #(R’’) < n, then R’’ = R’, where #(.) denotes the cardinality of a set, and n is a parameter.
-
+  if (R2.length <= MIN_SAMPLES_FOR_POSITION) R2 = R1
   // Step 4: Compute the similarity, the Manhattan distance, between the fingerprint given and all the fingerprints in R’’.
   R2.forEach(sample => { sample.similarity = manhattanDistance(sample.fingerprint, locationFingerprint) })
   // Step 5: Take the k1 samples in R’’ that are the most similar to fp0. (The ones with the smaller similarity, TODO check this)
